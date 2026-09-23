18e4124a490d1d0b5ab3946501392c2f7f4b4b6d
Sanjay Krishna N <krisanjay16@gmail.com>
2026-09-23 07:54:35 +0530

    File header and author credits added

--- src/main/java/org/cloud/config/springconfigserver/SpringConfigServerApplication.java
+++ src/main/java/org/cloud/config/springconfigserver/SpringConfigServerApplication.java
@@ -1,5 +1,13 @@
 package org.cloud.config.springconfigserver;
 
+/**
+ * ------------------------------------------------------------------------
+ * Author   : Sanjay Krishna Narayanan
+ * Created  : 9/23/26
+ * Version  : 1.0
+ * ------------------------------------------------------------------------
+ */
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
